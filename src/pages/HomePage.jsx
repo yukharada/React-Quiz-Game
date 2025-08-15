be6c32918fc6f8ceff7dd8edd10fe4795d2a71c9
yukharada <yuuki20011126harada@gmail.com>
2025-08-15 16:43:26 +0900

    TOPページ作成

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom"
+import { ROUTES } from "../const"
+
 export default function HomePage() {
     return (
-        <div>HomePage</div>
+        <>
+            <h1>Quiz App</h1>
+            <Link to={ROUTES.QUIZ}>Start!</Link>
+        </>
     )
 }
