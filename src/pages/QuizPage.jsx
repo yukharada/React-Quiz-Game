f11bff92fe658af2f9700518a0748c6a8f6a9468
yukharada <yuuki20011126harada@gmail.com>
2025-08-20 13:23:15 +0900

    正誤判定ロジック作成

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,24 +1,48 @@
+import { useEffect, useState } from "react";
 import Button from "../components/Button/Button";
 import Display from "../components/Display/Display"
 import quizData from "../data/quiz"
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../const"
 
 export default function QuizPage() {
-    const quizIndex = 0;
+    const [quizIndex, setQuizIndex] = useState(0);
+    const [answerLogs, setAnswerLogs] = useState([]);
+    const navigation = useNavigate();
+    const MAX_QUIZ_LEN = quizData.length;
+
     const handleClick = (clickidIndex) => {
-        console.log("clickedIndex:", clickidIndex);
+        if (clickidIndex === quizData[quizIndex].answerIndex) {
+            setAnswerLogs(prev => [...prev, true])
+        } else {
+            setAnswerLogs(prev => [...prev, false])
+        }
+        setQuizIndex(prev => prev + 1);
     }
+
+    useEffect(() => {
+        if (answerLogs.length === MAX_QUIZ_LEN) {
+            const correctNum = answerLogs.filter(answer => answer === true)
+            navigation(ROUTES.RESULT, {
+                state: {
+                    maxQuizLen: MAX_QUIZ_LEN,
+                    correctNum: correctNum
+                }
+            });
+        }
+    }, [answerLogs, MAX_QUIZ_LEN, navigation]);
+
     return (
         <>
-            <Display>
-                {`Q1. ${quizData[quizIndex].question}`}
-            </Display>
-            {
-                quizData[quizIndex].options.map((option, index) => {
-                    return <Button key={`option-${index}`} onClick={() => handleClick(index)}>
+            {quizData[quizIndex] && <Display>{`Q${quizIndex + 1}. ${quizData[quizIndex].question}`}</Display>}
+            <br />
+            {quizData[quizIndex] && quizData[quizIndex].options.map((option, index) => {
+                return (
+                    <Button key={`option-${index}`} onClick={() => handleClick(index)}>
                         {option}
                     </Button>
-                })
-            }
+                );
+            })}
         </>
     )
 }
